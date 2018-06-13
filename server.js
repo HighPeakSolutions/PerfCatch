0bac4c20d8074114f278deb97e73ae3fdb72494b
Scott <scottmtraver@gmail.com>
2018-06-12 20:48:34 -0600

    Add package and sequelize

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
+require('dotenv').config();
 const http = require('http');
 
 const hostname = 'localhost';
 const port = 5050;
+
+// Database Configuration
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
+  host: process.env.DBHOST,
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+});
+
+// Test Connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
