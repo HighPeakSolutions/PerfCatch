45da1e9e05d683229006783a0379989fcb601788
Scott <scottmtraver@gmail.com>
2018-06-12 21:06:39 -0600

    Basic insertion to postgres table

--- server.js
+++ server.js
@@ -7,35 +7,53 @@ const port = 5050;
 // Database Configuration
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASS, {
-  host: process.env.DBHOST,
-  dialect: 'postgres',
-  operatorsAliases: false,
+    host: process.env.DBHOST,
+    dialect: 'postgres',
+    operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
 
 });
 
 // Test Connection
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
+// first define the model
+const PerfRecord = sequelize.define('performance', {
+    url: Sequelize.STRING,
+    perfobj: Sequelize.JSON,
+    date: Sequelize.TIME
+})
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Performance World\n');
+    const task = PerfRecord.build(
+        {
+            url: 'testurl',
+            perfobj: {},
+            date: new Date()
+        }
+    )
+    task.save().then(function () {
+        console.log('here')
+    })
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Performance World\n');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
